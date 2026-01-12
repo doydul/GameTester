07d8aa469e0ac072e4b12963b1048cfc9ad10a8c
BATMAN\lewis <lewis.wrigley@gmail.com>
2026-01-12 16:26:50 +0000

    update game rules

--- src/cardData.js
+++ src/cardData.js
@@ -5,16 +5,16 @@ export const initialDeck = [
   { type: 'Action', title: "meditation", description: "gain 3 mana", count: 4 },
   { type: 'Action', title: "mugging", description: "fight another player, winner takes 4 gold from loser, loser takes 2 damage", count: 2 },
   { type: 'Action', title: "duel", description: "fight another player, loser takes 4 damage", count: 2 },
-  { type: 'Action', title: "lay low", description: "ignore the effects of an action card played by another player", count: 2 },
-  { type: 'Action', title: "sneaky stab", description: "add 2 to strength", count: 4 },
+  { type: 'Action', title: "lay low", description: "Reaction: ignore the effects of an action card played by another player", count: 2 },
+  { type: 'Action', title: "sneaky stab", description: "Reaction: add 2 to strength", count: 4 },
   { type: 'Action', title: "scout", description: "discard up to 2 adventures from the row and replace them", count: 3 },
   { type: 'Action', title: "maintenance", description: "discard all damage markers from 1 weapon", count: 3 },
   { type: 'Action', title: "scavenging", description: "take a card from the discard pile", count: 2 },
-  { type: 'Action', title: "sabotage", description: "play at the start of another players turn; they cannot claim adventures this turn", count: 2 },
+  { type: 'Action', title: "sabotage", description: "Reaction: play at the start of another players turn; they cannot claim adventures this turn", count: 2 },
   
   // Items
   { type: 'Item', title: "rogue's cowl", cost: '7 Gold', description: "draw an extra card at start of turn", count: 1 },
-  { type: 'Item', title: "rogue's cowl", cost: '6 Gold', description: "once per turn after you play an action, return it to your hand", count: 1 },
+  { type: 'Item', title: "Thief's tools", cost: '6 Gold', description: "once per turn after you play an action card (not a reaction), return it to your hand", count: 1 },
   { type: 'Item', title: "wizard's robe", cost: '5 Gold', description: "you may gain 1 mana at start of turn", count: 1 },
   { type: 'Item', title: "looking glass", cost: '1 Gold', description: "on your turn you may look at the top card of the adventure deck", count: 1 },
   { type: 'Item', title: "berserker pelts", cost: '4 Gold', description: "your strength is doubled. damage you take is doubled", count: 1 },
@@ -23,7 +23,7 @@ export const initialDeck = [
   { type: 'Item', title: "belt of strength", cost: '2 Gold', description: "+1 strength", count: 2 },
   { type: 'Item', title: "leather jerkin", cost: '2 Gold', description: "+3 max hp", count: 2 },
   { type: 'Item', title: "full plate", cost: '6 Gold', description: "+7 max hp", count: 2 },
-  { type: 'Item', title: "crown of nobility", cost: '25 Gold', description: "8 victory points", count: 1 },
+  { type: 'Item', title: "crown of nobility", cost: '25 Gold', description: "7 Victory Points. Cannot be played for free", count: 1 },
   { type: 'Item', title: "potion of rejuvination", cost: '4 Gold', description: "if you would die, discard damage equal to half your max hp (rounded down) instead, and discard this item", count: 2 },
   
   // Weapons
@@ -37,29 +37,30 @@ export const initialDeck = [
   
   // Spells
   { type: 'Spell', title: "fireball", cost: '3 Mana', description: "add 6 to strength", count: 3 },
-  { type: 'Spell', title: "counterspell", cost: '3 Mana', description: "counter a spell or scroll", count: 2 },
+  { type: 'Spell', title: "counterspell", cost: '3 Mana', description: "Reaction: counter a spell or scroll", count: 2 },
   { type: 'Spell', title: "turn to rust", cost: '3 Mana', description: "destroy a weapon that has at least 1 damage on it", count: 2 },
   { type: 'Spell', title: "zap", cost: '4 Mana', description: "deal 3 damage to a player", count: 2 },
   { type: 'Spell', title: "divination", cost: '3 Mana', description: "draw 3 cards", count: 2 },
   { type: 'Spell', title: "animate dead", cost: '4 Mana', description: "take up to 2 cards from the discard pile", count: 2 },
   { type: 'Spell', title: "destruction", cost: 'X Mana', description: "destroy an item. mana costs is equal to the target's gold cost", count: 2 },
-  { type: 'Spell', title: "reincarnation", cost: '5 Mana', description: "play when you die. remove all damage and ignore all usual effects of death", count: 1 },
+  { type: 'Spell', title: "reincarnation", cost: '5 Mana', description: "Reaction: play when you die. remove all damage and ignore all usual effects of death", count: 1 },
   { type: 'Spell', title: "god mode", cost: '6 Mana', description: "claim any adventure without comparing strength or paying it's costs", count: 1 },
   { type: 'Spell', title: "power word: death", cost: '8 Mana', description: "deal damage to a player equal to their maximum hp. this spell cannot be countered", count: 1 },
   
   // Scrolls
-  { type: 'Spell - Scroll', title: "cone of frost", cost: '2 Mana', description: "add 2 to strength", count: 2 },
+  { type: 'Spell - Scroll', title: "cone of frost", cost: '2 Mana', description: "Reaction: add 2 to strength", count: 2 },
   { type: 'Spell - Scroll', title: "life drain", cost: '2 Mana', description: "deal 1 damage to another player and remove 1 damage from yourself", count: 2 },
   { type: 'Spell - Scroll', title: "healing hands", cost: '2 Mana', description: "remove 3 damage", count: 2 },
   { type: 'Spell - Scroll', title: "transmute", cost: '2 Mana', description: "discard an item from hand to receive gold equal to half it's cost (rounded down), or discard an item from your inventory for it's full cost", count: 2 },
-  { type: 'Spell - Scroll', title: "prismatic ray", cost: '4 Mana', description: "add 4 to strength", count: 2 },
+  { type: 'Spell - Scroll', title: "prismatic ray", cost: '4 Mana', description: "Reaction: add 4 to strength", count: 2 },
   
   // Misc
   { title: "Shuffle", description: "shuffle the discard pile (along with this card) into the main deck, then draw a card to replace this one", count: 1 },
 ];
 
 export const initialAdventureDeck = [
-  { type: 'Adventure', title: "arcane library", cost: '1 Strength, Discard 1 card', description: "1 Victory Point, Draw 2 Cards, 5 Mana", count: 3 },
+  { type: 'Adventure', title: "An Audience with the King", cost: '15 Gold, Discard 3 cards', description: "6 Victory Point", count: 1 },
+  { type: 'Adventure', title: "Arcane Library", cost: 'Discard 2 cards', description: "1 Victory Point, Draw 2 Cards, 5 Mana", count: 3 },
   { type: 'Adventure', title: "labyrinth", cost: '3 Strength, Discard 6 cards', description: "3 Victory Points, play 1 item from hand for free", count: 2 },
   { type: 'Adventure', title: "journey through the enchanted forest", cost: '4 Strength', description: "1 Victory Points, 2g, 3m", count: 3 },
   { type: 'Adventure', title: "goblin nest", cost: '3 Strength', description: "1 Victory Points, 3 Gold", count: 3 },
@@ -78,7 +79,7 @@ export const initialAdventureDeck = [
 export const characters = [
   { type: 'Character', title: "Warrior", stats: "Strength: 4\nMax Health: 16\nMana: 0", ability: "You take -1 damage from enemy spells and actions", count: 1 },
   { type: 'Character', title: "Rogue", stats: "Strength: 2\nMax Health: 9\nMana: 2", ability: "You may perform 3 actions per turn", count: 1 },
-  { type: 'Character', title: "Wizard", stats: "Strength: 1\nMax Health: 7\nMana: 3", ability: "The first time you play a spell each turn, draw a card", count: 1 },
+  { type: 'Character', title: "Wizard", stats: "Strength: 1\nMax Health: 7\nMana: 3", ability: "The first time you play a spell (not a reaction) each turn, draw a card", count: 1 },
   { type: 'Character', title: "Fighter", stats: "Strength: 3\nMax Health: 12\nMana: 1", ability: "+3 Strength when using 2 weapons at once", count: 1 },
   { type: 'Character', title: "Arcanist", stats: "Strength: 1\nMax Health: 8\nMana: 4", ability: "you only have to discard mana down to your limit (and suffer mana burn) at the end of your turn", count: 1 },
   { type: 'Character', title: "Ranger", stats: "Strength: 1\nMax Health: 8\nMana: 3", ability: "All your weapons have +1 durability", count: 1 },
