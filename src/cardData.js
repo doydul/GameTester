4df1a08422d1ebceb210892d0de628821ac5aeeb
BATMAN\lewis <lewis.wrigley@gmail.com>
2026-01-14 15:50:07 +0000

    more balancing changes

--- src/cardData.js
+++ src/cardData.js
@@ -1,58 +1,52 @@
 export const initialDeck = [
   // Actions
-  { type: 'Action', title: "skullduggery", description: "gain 2 gold", count: 4 },
-  { type: 'Action', title: "hard labour", cost: '2 damage', description: "gain 3 gold", count: 4 },
-  { type: 'Action', title: "meditation", description: "gain 1 mana", count: 4 },
-  { type: 'Action', title: "attack", description: "fight another player. for each point the winner wins by (to a maximum of 5), they may deal 1 damage or take 1 Gold from the loser", count: 3 },
-  // { type: 'Action', title: "lay low", description: "Reaction: ignore the effects of an action card played by another player", count: 2 },
-  { type: 'Action', title: "sneaky stab", description: "Reaction: add 2 to strength", count: 4 },
-  { type: 'Action', title: "scout", description: "discard up to 2 adventures from the row and replace them", count: 3 },
-  { type: 'Action', title: "maintenance", description: "discard all damage markers from 1 weapon", count: 3 },
-  { type: 'Action', title: "scavenging", description: "take a card from the discard pile", count: 2 },
-  // { type: 'Action', title: "sabotage", description: "Reaction: play at the start of another players turn; they cannot claim adventures this turn, and they take 1 damage", count: 2 },
+  { type: 'Action', title: "Skullduggery", description: "Gain 2 Gold", count: 5 },
+  { type: 'Action', title: "Hard Labour", cost: '2 Damage', description: "Gain 3 Gold", count: 4 },
+  { type: 'Action', title: "Meditation", description: "Gain 1 Mana", count: 4 },
+  { type: 'Action', title: "Attack", description: "Fight another player. For each point the winner wins by (to a maximum of 5), they may deal 1 Damage or take 1 Gold from the loser", count: 3 },
+  { type: 'Action', title: "Sneaky Stab", description: "Reaction: add 2 to Strength", count: 5 },
+  { type: 'Action', title: "Scout", description: "Discard up to 2 Adventures from the row and replace them", count: 4 },
+  { type: 'Action', title: "Maintenance", description: "Discard all Damage markers from 1 Weapon", count: 3 },
+  { type: 'Action', title: "Scavenging", description: "Take 1 Card from the discard pile", count: 3 },
+  { type: 'Action', title: "Brilliancy", description: "Draw 2 Cards", count: 3 },
   
   // Items
-  { type: 'Item', title: "rogue's cowl", cost: '10 Gold', description: "draw an extra card at start of turn", count: 2 },
-  { type: 'Item', title: "wizard's robe", cost: '12 Gold', description: "gain 1 mana at start of turn", count: 2 },
-  { type: 'Item', title: "arcane amulet", cost: '4 Gold', description: "+2 max mana", count: 2 },
-  { type: 'Item', title: "arcane amulet", cost: '4 Gold', description: "+2 max mana", count: 2 },
-  { type: 'Item', title: "arcane amulet", cost: '4 Gold', description: "+2 max mana", count: 2 },
-  // { type: 'Item', title: "Thief's tools", cost: '6 Gold', description: "once per turn after you play an action card (not a reaction), return it to your hand", count: 1 },
-  // { type: 'Item', title: "looking glass", cost: '1 Gold', description: "on your turn you may look at the top card of the adventure deck", count: 1 },
-  // { type: 'Item', title: "Berzerker Pelts", cost: '3 Gold', description: "Your strength is doubled. Damage you take is doubled", count: 1 },
-  { type: 'Item', title: "magic crystal", cost: '2 Gold', description: "+1 max mana", count: 3 },
-  { type: 'Item', title: "arcane amulet", cost: '4 Gold', description: "+2 max mana", count: 2 },
-  { type: 'Item', title: "belt of strength", cost: '3 Gold', description: "+1 strength", count: 2 },
-  { type: 'Item', title: "leather jerkin", cost: '2 Gold', description: "+2 max hp", count: 3 },
-  { type: 'Item', title: "full plate", cost: '4 Gold', description: "+4 max hp", count: 2 },
-  // { type: 'Item', title: "potion of rejuvination", cost: '4 Gold', description: "if you would die, discard damage equal to half your max hp (rounded down) instead, and discard this item", count: 2 },
+  { type: 'Item', title: "Rogue's Cowl", cost: '10 Gold', description: "Draw 1 extra Card at start of turn", count: 2 },
+  { type: 'Item', title: "wizard's robe", cost: '12 Gold', description: "Gain 1 Mana at start of turn", count: 2 },
+  { type: 'Item', title: "Thief's Tools", cost: '4 Gold', description: "When scheming, you may draw Cards from the deck or discard pile", count: 2 },
+  { type: 'Item', title: "Hidden Weapons", cost: '6 Gold', description: "When you play an Action card other than 'Attack', double it's rewards", count: 2 },
+  { type: 'Item', title: "", cost: '2 Gold', description: "+2 max hand size", count: 3 },
+  { type: 'Item', title: "Magic Crystal", cost: '2 Gold', description: "+1 max Mana", count: 3 },
+  { type: 'Item', title: "Arcane Amulet", cost: '4 Gold', description: "+2 max Mana", count: 2 },
+  { type: 'Item', title: "Belt of Strength", cost: '3 Gold', description: "+1 Strength", count: 2 },
+  { type: 'Item', title: "Leather Jerkin", cost: '2 Gold', description: "+2 max hp", count: 3 },
+  { type: 'Item', title: "Full Plate", cost: '4 Gold', description: "+4 max hp", count: 2 },
+  { type: 'Item', title: "Potion of Healing", cost: '5 Gold', description: "Reaction: after taking damage, discard this Item to heal damage equal to half your max HP (rounded down). NB: this happens before checking to see if you're dead", count: 2 },
   
   // Weapons
-  { type: 'Item - Weapon', title: "dagger", cost: '2 Gold', description: "2 strength", durability: 2, count: 3 },
-  // { type: 'Item - Weapon', title: "masterwork dagger", cost: '3 Gold', description: "2 strength", durability: 4, count: 1 },
-  { type: 'Item - Weapon', title: "sword", cost: '4 Gold', description: "3 strength", durability: 2, count: 3 },
-  // { type: 'Item - Weapon', title: "short bow", cost: '6 Gold', description: "2 strength, isn't discarded when durability is reached", durability: 2, count: 2 },
-  { type: 'Item - Weapon', title: "great axe", cost: '8 Gold', description: "6 strength, double handed", durability: 2, count: 3 },
-  { type: 'Item - Weapon', title: "battle axe", cost: '8 Gold', description: "5 strength", durability: 2, count: 3 },
-  { type: 'Item - Weapon', title: "bastard sword", cost: '10 Gold', description: "6 strength", durability: 2, count: 2 },
-  { type: 'Item - Weapon', title: "claymore", cost: '12 Gold', description: "7 strength", durability: 2, count: 2 },
-  { type: 'Item - Weapon', title: "falcon blade", cost: '14 Gold', description: "8 strength", durability: 2, count: 2 },
-  { type: 'Item - Weapon', title: "demon edge", cost: '16 Gold', description: "9 strength", durability: 2, count: 1 },
-  { type: 'Item - Weapon', title: "reaver", cost: '19 Gold', description: "10 strength", durability: 2, count: 1 },
-  { type: 'Item - Weapon', title: "heavenly halberd", cost: '22 Gold', description: "11 strength", durability: 2, count: 1 }, 
-  { type: 'Item - Weapon', title: "scythe of vyse", cost: '25 Gold', description: "12 strength, passive: +1 max mana", durability: 2, count: 1 },
-  { type: 'Item - Weapon', title: "abyssal blade", cost: '28 Gold', description: "13 strength", durability: 2, count: 1 },
+  { type: 'Item - Weapon', title: "Dagger", cost: '2 Gold', description: "2 Strength", durability: 2, count: 3 },
+  { type: 'Item - Weapon', title: "Sword", cost: '4 Gold', description: "3 Strength", durability: 2, count: 3 },
+  { type: 'Item - Weapon', title: "Great Axe", cost: '8 Gold', description: "6 Strength, double handed", durability: 2, count: 3 },
+  { type: 'Item - Weapon', title: "Battle Axe", cost: '8 Gold', description: "5 Strength", durability: 2, count: 3 },
+  { type: 'Item - Weapon', title: "Bastard Sword", cost: '10 Gold', description: "6 Strength", durability: 2, count: 2 },
+  { type: 'Item - Weapon', title: "Claymore", cost: '12 Gold', description: "7 Strength", durability: 2, count: 2 },
+  { type: 'Item - Weapon', title: "Falcon Blade", cost: '14 Gold', description: "8 Strength", durability: 2, count: 2 },
+  { type: 'Item - Weapon', title: "Demon Edge", cost: '16 Gold', description: "9 Strength", durability: 2, count: 1 },
+  { type: 'Item - Weapon', title: "Reaver", cost: '19 Gold', description: "10 Strength", durability: 2, count: 1 },
+  { type: 'Item - Weapon', title: "Heavenly Halberd", cost: '22 Gold', description: "11 Strength", durability: 2, count: 1 }, 
+  { type: 'Item - Weapon', title: "Scythe of Vyse", cost: '25 Gold', description: "12 Strength, passive: +1 max Mana", durability: 2, count: 1 },
+  { type: 'Item - Weapon', title: "Abyssal Blade", cost: '28 Gold', description: "13 Strength", durability: 2, count: 1 },
   
   // Spells
-  { type: 'Spell', title: "Fire Bolt", cost: '1/2/3/4 Mana', description: "Reaction: add 3/6/9/13 strength", count: 8 },
-  { type: 'Spell', title: "Divination", cost: '1/2 Mana', description: "Draw 2/5 cards from the deck or discard pile", count: 3 },
+  { type: 'Spell', title: "Fire Bolt", cost: '1/2/3/4 Mana', description: "Reaction: add 3/6/9/13 Strength", count: 8 },
+  { type: 'Spell', title: "Divination", cost: '1/2 Mana', description: "Draw 2/4 Cards from the deck or discard pile", count: 3 },
   { type: 'Spell', title: "Transmute", cost: '1 Mana', description: "Discard an item from hand to receive 4 Gold", count: 3 },
-  { type: 'Spell', title: "Cure Wounds", cost: '1/2 Mana', description: "Remove 3/5 damage", count: 3 },
-  { type: 'Spell', title: "Counterspell", cost: '1 Mana', description: "Reaction: counter a spell", count: 4 },
+  { type: 'Spell', title: "Cure Wounds", cost: '1/2 Mana', description: "Remove 3/5 Damage", count: 3 },
+  { type: 'Spell', title: "Counterspell", cost: '1 Mana', description: "Reaction: counter a Spell", count: 4 },
   // { type: 'Spell', title: "rust", cost: '1 Mana', description: "destroy a weapon that has at least 1 damage on it", count: 2 },
-  { type: 'Spell', title: "Zap", cost: '1/2/3 Mana', description: "Deal 2/3/4 damage to a player", count: 4 },
+  { type: 'Spell', title: "Zap", cost: '1/2/3 Mana', description: "Deal 2/3/4 Damage to a player", count: 4 },
   // { type: 'Spell', title: "animate dead", cost: '3 Mana', description: "deal 2 damage to a player then take up to 2 cards from the discard pile", count: 2 },
-  { type: 'Spell', title: "Destruction", cost: '2 Mana', description: "Add a damage token to a weapon or destroy a non-weapon item", count: 3 },
+  { type: 'Spell', title: "Destruction", cost: '2 Mana', description: "Add a Damage token to a Weapon or destroy a non-Weapon Item", count: 3 },
   // { type: 'Spell', title: "Oblivion", cost: '5 Mana', description: "deal damage to a player equal to their maximum hp. this spell cannot be countered", count: 1 },
   
   // Misc
@@ -63,15 +57,15 @@ export const initialDeck = [
 Costs:
 gold - bribery
 damage - enduring hazardous environments; poison
-mana - overcoming magical barriers/traps; utilising intellect
+Mana - overcoming magical barriers/traps; utilising intellect
 discarding cards - utilising cunning/skill; taking large amounts of time
-strength - fighting enemies/monsters/animals
+Strength - fighting enemies/monsters/animals
 sacrificing items - acid; going to places where thievery is common (pirate hideouts etc)
 
 Rewards:
 gold - finding treasure healing
 damage - visiting kind entities, fairies, benevolent deities etc
-mana - magical/enchanted surroundings; fighting magical beings
+Mana - magical/enchanted surroundings; fighting magical beings
 drawing cards - gaining knowledge/intel
 drawing cards from discard pile - plundering crypts; fighting undead
 causing other players to take damage - large scale battles
@@ -123,14 +117,18 @@ export const characters = [
 
 export function createDeck() {
   const deck = [];
+  const cardTypes = {};
   initialDeck.forEach((card, index) => {
     for (let i = 0; i < card.count; i++) {
+      if (!cardTypes[card.type]) cardTypes[card.type] = 0;
+      cardTypes[card.type] += 1;
       deck.push({
         ...card,
         id: `card-${index}-${i}-${Date.now()}`,
       });
     }
   });
+  console.log(cardTypes);
   return deck;
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,51 @@
 import { updateCardDamage } from '../firebase.js';
 
+// Keyword highlighting configuration
+const keywordStyles = {
+  item: 'keyword-item',
+  items: 'keyword-item',
+  strength: 'keyword-strength',
+  spell: 'keyword-spell',
+  spells: 'keyword-spell',
+  mana: 'keyword-mana',
+  weapon: 'keyword-weapon',
+  weapons: 'keyword-weapon',
+  gold: 'keyword-gold',
+  action: 'keyword-action',
+  actions: 'keyword-action',
+  damage: 'keyword-damage',
+  card: 'keyword-card',
+  cards: 'keyword-card',
+  "victory point": 'keyword-vp',
+  "victory points": 'keyword-vp',
+  vp: 'keyword-vp',
+  vps: 'keyword-vp',
+};
+
+// Create regex pattern for all keywords (case-insensitive, word boundaries)
+const keywordPattern = new RegExp(
+  `\\b(${Object.keys(keywordStyles).join('|')})\\b`,
+  'gi'
+);
+
+function highlightKeywords(text) {
+  if (!text) return text;
+
+  const parts = text.split(keywordPattern);
+
+  return parts.map((part, index) => {
+    const lowerPart = part.toLowerCase();
+    if (keywordStyles[lowerPart]) {
+      return (
+        <span key={index} className={keywordStyles[lowerPart]}>
+          {part}
+        </span>
+      );
+    }
+    return part;
+  });
+}
+
 export function Card({ card, onPlay, onTake, onDiscard, onMoveToInventory, cardDamage }) {
   const currentDamage = cardDamage?.[card.id] ?? 0;
 
@@ -12,15 +58,15 @@ export function Card({ card, onPlay, onTake, onDiscard, onMoveToInventory, cardD
       <div className="card-header">
         <span className="card-title">{card.title}</span>
         {card.type && (
-          <span className="card-type">{card.type}</span>
+          <span className="card-type">{highlightKeywords(card.type)}</span>
         )}
       </div>
       {card.cost && (
         <div className="card-cost-container">
-          <span className="card-cost">{card.cost}</span>
+          <span className="card-cost">{highlightKeywords(card.cost)}</span>
         </div>
       )}
-      <div className="card-description">{card.description}</div>
+      <div className="card-description">{highlightKeywords(card.description)}</div>
       {card.durability && (
         <div className="card-durability">
           <span className="durability-label">Durability:</span>
