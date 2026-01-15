e8b4c63567c28d9ebad8d76fef7faf0954e2005b
BATMAN\lewis <lewis.wrigley@gmail.com>
2026-01-15 23:35:46 +0000

    total adventure rework

--- src/cardData.js
+++ src/cardData.js
@@ -8,46 +8,40 @@ export const initialDeck = [
   { type: 'Action', title: "Scout", description: "Discard up to 2 Adventures from the row and replace them", count: 4 },
   { type: 'Action', title: "Maintenance", description: "Discard all Damage markers from 1 Weapon", count: 3 },
   { type: 'Action', title: "Scavenging", description: "Take 1 Card from the discard pile", count: 3 },
-  { type: 'Action', title: "Brilliancy", description: "Draw 2 Cards", count: 3 },
+  { type: 'Action', title: "Steal", description: "Look at another player's hand and take 1 card from it", count: 2 },
   
   // Items
   { type: 'Item', title: "Rogue's Cowl", cost: '10 Gold', description: "Draw 1 extra Card at start of turn", count: 2 },
   { type: 'Item', title: "wizard's robe", cost: '12 Gold', description: "Gain 1 Mana at start of turn", count: 2 },
-  { type: 'Item', title: "Thief's Tools", cost: '4 Gold', description: "When scheming, you may draw Cards from the deck or discard pile", count: 2 },
-  { type: 'Item', title: "Hidden Weapons", cost: '6 Gold', description: "When you play an Action card other than 'Attack', double it's rewards", count: 2 },
-  { type: 'Item', title: "", cost: '2 Gold', description: "+2 max hand size", count: 3 },
-  { type: 'Item', title: "Magic Crystal", cost: '2 Gold', description: "+1 max Mana", count: 3 },
-  { type: 'Item', title: "Arcane Amulet", cost: '4 Gold', description: "+2 max Mana", count: 2 },
-  { type: 'Item', title: "Belt of Strength", cost: '3 Gold', description: "+1 Strength", count: 2 },
-  { type: 'Item', title: "Leather Jerkin", cost: '2 Gold', description: "+2 max hp", count: 3 },
-  { type: 'Item', title: "Full Plate", cost: '4 Gold', description: "+4 max hp", count: 2 },
-  { type: 'Item', title: "Potion of Healing", cost: '5 Gold', description: "Reaction: after taking damage, discard this Item to heal damage equal to half your max HP (rounded down). NB: this happens before checking to see if you're dead", count: 2 },
+  { type: 'Item', title: "Thief's Tools", cost: '4 Gold', description: "When scheming, you may discard 2 cards to draw 1 from the discard pile instead of the usual action", count: 2 },
+  // { type: 'Item', title: "Hidden Weapons", cost: '5 Gold', description: "When you play an Action card other than 'Attack' or 'Hard Labour', double it's effects", count: 2 },
+  { type: 'Item', title: "Arcane Amulet", cost: '2 Gold', description: "+2 max Mana", count: 2 },
+  { type: 'Item', title: "Leather Armour", cost: '2 Gold', description: "+3 max hp", count: 3 },
+  { type: 'Item', title: "Potion of Healing", cost: '5 Gold', description: "Reaction: after taking damage, you may discard this Item to heal damage equal to half your max HP (rounded down). NB: this happens before checking to see if you're dead", count: 2 },
+  // { type: 'Item', title: "Hideout", cost: '6 Gold', description: "You cannot be targeted by other player's Actions or Spells", count: 2 },
   
   // Weapons
   { type: 'Item - Weapon', title: "Dagger", cost: '2 Gold', description: "2 Strength", durability: 2, count: 3 },
   { type: 'Item - Weapon', title: "Sword", cost: '4 Gold', description: "3 Strength", durability: 2, count: 3 },
-  { type: 'Item - Weapon', title: "Great Axe", cost: '8 Gold', description: "6 Strength, double handed", durability: 2, count: 3 },
+  { type: 'Item - Weapon', title: "Halberd", cost: '6 Gold', description: "4 Strength", durability: 2, count: 3 },
   { type: 'Item - Weapon', title: "Battle Axe", cost: '8 Gold', description: "5 Strength", durability: 2, count: 3 },
+  { type: 'Item - Weapon', title: "Two Handed Axe", cost: '8 Gold', description: "7 Strength", durability: 2, count: 2 },
   { type: 'Item - Weapon', title: "Bastard Sword", cost: '10 Gold', description: "6 Strength", durability: 2, count: 2 },
   { type: 'Item - Weapon', title: "Claymore", cost: '12 Gold', description: "7 Strength", durability: 2, count: 2 },
-  { type: 'Item - Weapon', title: "Falcon Blade", cost: '14 Gold', description: "8 Strength", durability: 2, count: 2 },
-  { type: 'Item - Weapon', title: "Demon Edge", cost: '16 Gold', description: "9 Strength", durability: 2, count: 1 },
-  { type: 'Item - Weapon', title: "Reaver", cost: '19 Gold', description: "10 Strength", durability: 2, count: 1 },
-  { type: 'Item - Weapon', title: "Heavenly Halberd", cost: '22 Gold', description: "11 Strength", durability: 2, count: 1 }, 
-  { type: 'Item - Weapon', title: "Scythe of Vyse", cost: '25 Gold', description: "12 Strength, passive: +1 max Mana", durability: 2, count: 1 },
-  { type: 'Item - Weapon', title: "Abyssal Blade", cost: '28 Gold', description: "13 Strength", durability: 2, count: 1 },
+  { type: 'Item - Weapon', title: "Falcon Sabre", cost: '14 Gold', description: "8 Strength", durability: 2, count: 2 },
+  { type: 'Item - Weapon', title: "Titan Blade", cost: '14 Gold', description: "10 Strength, double handed", durability: 2, count: 2 },
   
   // Spells
-  { type: 'Spell', title: "Fire Bolt", cost: '1/2/3/4 Mana', description: "Reaction: add 3/6/9/13 Strength", count: 8 },
+  { type: 'Spell', title: "Magic Missile", cost: '1/2/3 Mana', description: "Reaction: add 4/9/12 Strength", count: 4 },
+  { type: 'Spell', title: "Fireball", cost: '2/3/4 Mana', description: "Reaction: add 8/13/17 Strength", count: 4 },
   { type: 'Spell', title: "Divination", cost: '1/2 Mana', description: "Draw 2/4 Cards from the deck or discard pile", count: 3 },
-  { type: 'Spell', title: "Transmute", cost: '1 Mana', description: "Discard an item from hand to receive 4 Gold", count: 3 },
+  { type: 'Spell', title: "Transmute", cost: '2 Mana', description: "Destroy an Item. It's owner receives Gold equal to half it's value, rounded down", count: 2 },
   { type: 'Spell', title: "Cure Wounds", cost: '1/2 Mana', description: "Remove 3/5 Damage", count: 3 },
-  { type: 'Spell', title: "Counterspell", cost: '1 Mana', description: "Reaction: counter a Spell", count: 4 },
-  // { type: 'Spell', title: "rust", cost: '1 Mana', description: "destroy a weapon that has at least 1 damage on it", count: 2 },
-  { type: 'Spell', title: "Zap", cost: '1/2/3 Mana', description: "Deal 2/3/4 Damage to a player", count: 4 },
-  // { type: 'Spell', title: "animate dead", cost: '3 Mana', description: "deal 2 damage to a player then take up to 2 cards from the discard pile", count: 2 },
-  { type: 'Spell', title: "Destruction", cost: '2 Mana', description: "Add a Damage token to a Weapon or destroy a non-Weapon Item", count: 3 },
-  // { type: 'Spell', title: "Oblivion", cost: '5 Mana', description: "deal damage to a player equal to their maximum hp. this spell cannot be countered", count: 1 },
+  { type: 'Spell', title: "Counterspell", cost: '1 Mana', description: "Reaction: counter a Spell. Gain 1 Mana", count: 4 },
+  { type: 'Spell', title: "Zap", cost: '1/2/3 Mana', description: "Deal 2/3/4 Damage to a player", count: 3 },
+  { type: 'Spell', title: "Meteor Storm", cost: '2/3/4 Mana', description: "Deal 2/3/5 Damage to each other player", count: 2 },
+  { type: 'Spell', title: "Destruction", cost: '2 Mana', description: "Add a Damage token to a Weapon or destroy a non-Weapon Item", count: 2 },
+  { type: 'Spell', title: "Stupify", cost: '1/2 Mana', description: "A player of your choice discards 2/5 Cards", count: 2 },
   
   // Misc
   { title: "Shuffle", description: "shuffle the discard pile (along with this card) into the main deck, then draw a card to replace this one", count: 1 },
@@ -70,22 +64,76 @@ drawing cards - gaining knowledge/intel
 drawing cards from discard pile - plundering crypts; fighting undead
 causing other players to take damage - large scale battles
 victory points - grand, noteworthy deeds
+repair a weapon - urban areas?
 
 Experimental Ideas:
-discard cards with some total gold value?
 discard certain number of cards of a certain type?
+destroy another player's item?
+all plauyers (including you) discard their most valuable item
+take mana from another player/players
 */
 
 export const initialAdventureDeck = [
-  { type: 'Adventure', victoryPoints: 1, title: "Investigate the Sunken Catacombs", cost: '2 Damage', description: "1 Victory Point, 3 Gold, 1 Card from deck or discard pile", count: 3 },
-  { type: 'Adventure', victoryPoints: 1, title: "Clear the Obsidian Watchtower", cost: '6 Strength', description: "1 Victory Point, 2 Mana, 2 Gold", count: 3 },
-  { type: 'Adventure', victoryPoints: 1, title: "Clear a Goblin Nest", cost: '7 Strength', description: "1 Victory Point, 5 Gold", count: 3 },
-  { type: 'Adventure', victoryPoints: 1, title: "Raid an Orc Camp", cost: '10 Strength', description: "1 Victory Point, 8 Gold", count: 3 },
-  { type: 'Adventure', victoryPoints: 2, title: "Trial of the Astral Guardians", cost: '13 Strength', description: "2 Victory Point, 3 Mana, 4 Gold", count: 2 },
-  { type: 'Adventure', victoryPoints: 4, title: "Slay an Ogre Chief", cost: '13 Strength', description: "4 Victory Point, 4 Gold", count: 2 },
-  { type: 'Adventure', victoryPoints: 5, title: "Hunt a Dire Basilisk", cost: '16 Strength', description: "5 Victory Point, 6 Gold", count: 2 },
-  { type: 'Adventure', victoryPoints: 8, title: "Discover the Codex of Eternity", cost: '19 Strength', description: "8 Victory Points, 4 Mana", count: 1 },
-  { type: 'Adventure', victoryPoints: 7, title: "Bring Back the Head of a Dragon", cost: '19 Strength', description: "7 Victory Point", count: 1 },
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '6 Strength', description: "1 VP, 7 Gold", count: 3 },
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '7 Strength', description: "1 VP, 8 Gold", count: 3 },
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '8 Strength', description: "1 VP, 9 Gold", count: 4 },
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '9 Strength', description: "1 VP, 10 Gold", count: 4 },
+  { type: 'Adventure', victoryPoints: 2, title: "", cost: '10 Strength', description: "2 VP, 11 Gold", count: 3 },
+  { type: 'Adventure', victoryPoints: 2, title: "", cost: '11 Strength', description: "2 VP, 12 Gold", count: 3 },
+  { type: 'Adventure', victoryPoints: 2, title: "", cost: '12 Strength', description: "2 VP, 13 Gold", count: 3 },
+  { type: 'Adventure', victoryPoints: 3, title: "", cost: '13 Strength', description: "3 VP, 14 Gold", count: 2 },
+  { type: 'Adventure', victoryPoints: 3, title: "", cost: '14 Strength', description: "3 VP, 15 Gold", count: 2 },
+  { type: 'Adventure', victoryPoints: 3, title: "", cost: '15 Strength', description: "3 VP, 16 Gold", count: 2 },
+  { type: 'Adventure', victoryPoints: 4, title: "", cost: '16 Strength', description: "4 VP, 17 Gold", count: 1 },
+  { type: 'Adventure', victoryPoints: 4, title: "", cost: '17 Strength', description: "4 VP, 18 Gold", count: 1 },
+  { type: 'Adventure', victoryPoints: 5, title: "", cost: '18 Strength', description: "5 VP, 19 Gold", count: 1 },
+  { type: 'Adventure', victoryPoints: 6, title: "", cost: '20 Strength', description: "8 VP, 10 Gold", count: 1 },
+  
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '4 Strength, 2 Item Cards', description: "1 VP, 4 Gold, 1 Mana", count: 3 },
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '1 Gold, 3 Cards, 2 Damage', description: "1 VP, 5 Gold, 2 Cards from deck or discard", count: 3 },
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '3 Damage, 4 Strength', description: "1 VP, 4 Gold, 3 Cards, 1 Mana", count: 4 },
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '9 Strength', description: "1 VP, 1 Gold, 1 Card, 3 Mana", count: 4 },
+  { type: 'Adventure', victoryPoints: 2, title: "", cost: '2 Spell Cards, 2 Mana', description: "2 VP, 10 Gold, 1 Card from deck or discard", count: 3 },
+  { type: 'Adventure', victoryPoints: 2, title: "", cost: '2 Gold, 5 Cards, 2 Damage', description: "2 VP, 3 Gold, 2 Cards, 2 Mana", count: 3 },
+  { type: 'Adventure', victoryPoints: 2, title: "", cost: '1 Mana, 8 Strength', description: "2 VP, 11 Gold, 1 Card from deck or discard", count: 3 },
+  { type: 'Adventure', victoryPoints: 3, title: "", cost: '4 Gold, 3 Action Cards, 2 Damage', description: "3 VP, 4 Cards, 3 Mana", count: 2 },
+  
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '2 Damage', description: "1 VP, 1 Card from deck or discard", count: 3 },
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '3 Damage', description: "1 VP, 1 Card from deck or discard, 1 Mana", count: 2 },
+  
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '3 Cards, 1 Damage', description: "1 VP, 5 Gold", count: 2 },
+  
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '3 Action Cards, 20 Strength', description: "10 VP, 4 Gold", count: 1 },
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '7 Cards, 12 Strength', description: "10 VP, 4 Gold", count: 1 },
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '3 Spell Cards, 5 Mana', description: "10 VP, 4 Gold", count: 1 },
+  { type: 'Adventure', victoryPoints: 1, title: "", cost: '3 Item Cards, 18 Gold', description: "10 VP, 4 Gold", count: 1 },
+  
+  // { type: 'Adventure', victoryPoints: 2, title: "Raid a Pirate Stronghold", cost: '7 Strength, 2 Damage, 1 Item worth 4 Gold or more', description: "2 Victory Points, 3 Cards, choose a player to discard their most valuable item", count: 2 },
+  // { type: 'Adventure', victoryPoints: 2, title: "Mend a Fractured Leyline", cost: '2 Damage, 2 Cards, 2 Mana', description: "2 Victory Point, take all Mana from other players", count: 2 },
+  
+  // { type: 'Adventure', victoryPoints: 1, title: "Investigate the Sunken Catacombs", cost: '2 Damage', description: "1 Victory Point, 3 Gold, 1 Card from deck or discard pile", count: 3 },
+  // { type: 'Adventure', victoryPoints: 1, title: "Clear the Obsidian Watchtower", cost: '6 Strength', description: "1 Victory Point, 2 Mana, 2 Gold", count: 3 },
+  // { type: 'Adventure', victoryPoints: 1, title: "Clear a Goblin Nest", cost: '7 Strength', description: "1 Victory Point, 6 Gold", count: 3 },
+  // { type: 'Adventure', victoryPoints: 1, title: "Raid an Orc Camp", cost: '10 Strength', description: "1 Victory Point, 8 Gold", count: 3 },
+  // { type: 'Adventure', victoryPoints: 2, title: "Trial of the Astral Guardians", cost: '13 Strength', description: "2 Victory Point, 3 Mana, 4 Gold", count: 2 },
+  // { type: 'Adventure', victoryPoints: 4, title: "Slay an Ogre Chief", cost: '13 Strength', description: "4 Victory Point, 4 Gold", count: 2 },
+  // { type: 'Adventure', victoryPoints: 5, title: "Hunt a Dire Basilisk", cost: '16 Strength', description: "5 Victory Point, 6 Gold", count: 2 },
+  // { type: 'Adventure', victoryPoints: 7, title: "Bring Back the Head of a Dragon", cost: '19 Strength', description: "7 Victory Point", count: 1 },
+  // { type: 'Adventure', victoryPoints: 2, title: "Mend a Fractured Leyline", cost: '2 Damage, 2 Cards', description: "2 Victory Point, 1 Gold, 3 Mana", count: 2 },
+  // { type: 'Adventure', victoryPoints: 4, title: "Slay the Ashen Wyrn", cost: '11 Strength, 2 Damage, 1 Mana', description: "4 Victory Points, 5 Gold", count: 1 },
+  // { type: 'Adventure', victoryPoints: 6, title: "Brave the Void Between Worlds", cost: '10 Strength, Discard Hand', description: "6 Victory Points, 3 Gold, 1 Card", count: 1 },
+  // { type: 'Adventure', victoryPoints: 7, title: "Raise an Army for the Final Crusade", cost: '8 Strength, 1 Card, 14 Gold', description: "7 Victory Points, 2 Damage to other players", count: 1 },
+  // { type: 'Adventure', victoryPoints: 2, title: "Breach the Adamant Vault", cost: '5 Strength, 4 Cards, 2 Mana', description: "2 Victory Points, 12 Gold", count: 1 },
+  
+  // { type: 'Adventure', victoryPoints: 1, title: "Investigate the Sunken Catacombs", cost: '2 Damage', description: "1 Victory Point, 3 Gold, 1 Card from deck or discard pile", count: 3 },
+  // { type: 'Adventure', victoryPoints: 1, title: "Clear the Obsidian Watchtower", cost: '6 Strength', description: "1 Victory Point, 2 Mana, 2 Gold", count: 3 },
+  // { type: 'Adventure', victoryPoints: 1, title: "Clear a Goblin Nest", cost: '7 Strength', description: "1 Victory Point, 5 Gold", count: 3 },
+  // { type: 'Adventure', victoryPoints: 1, title: "Raid an Orc Camp", cost: '10 Strength', description: "1 Victory Point, 8 Gold", count: 3 },
+  // { type: 'Adventure', victoryPoints: 2, title: "Trial of the Astral Guardians", cost: '13 Strength', description: "2 Victory Point, 3 Mana, 4 Gold", count: 2 },
+  // { type: 'Adventure', victoryPoints: 4, title: "Slay an Ogre Chief", cost: '13 Strength', description: "4 Victory Point, 4 Gold", count: 2 },
+  // { type: 'Adventure', victoryPoints: 5, title: "Hunt a Dire Basilisk", cost: '16 Strength', description: "5 Victory Point, 6 Gold", count: 2 },
+  // { type: 'Adventure', victoryPoints: 8, title: "Discover the Codex of Eternity", cost: '19 Strength', description: "8 Victory Points, 4 Mana", count: 1 },
+  // { type: 'Adventure', victoryPoints: 7, title: "Bring Back the Head of a Dragon", cost: '19 Strength', description: "7 Victory Point", count: 1 },
   
   
   // { type: 'Adventure', victoryPoints: 1, title: "Visit the Grove of Kindly Fey", cost: '1 Mana', description: "1 Victory Point, all players heal 3 Damage", count: 2 },
@@ -96,7 +144,6 @@ export const initialAdventureDeck = [
   // { type: 'Adventure', victoryPoints: 2, title: "Face a stampede of War Beasts", cost: '7 Strength, 3 Damage', description: "2 Victory Point, 2 Damage to other players", count: 2 },
   // { type: 'Adventure', victoryPoints: 2, title: "Mend a Fractured Leyline", cost: '2 Damage, 2 Cards', description: "2 Victory Point, 1 Gold, 3 Mana", count: 2 },
   // { type: 'Adventure', victoryPoints: 3, title: "Trial of the Astral Guardians", cost: '5 Strength, 1 Card, 2 Mana', description: "3 Victory Point, 3 Gold, 2 Card", count: 2 },
-  // { type: 'Adventure', victoryPoints: 2, title: "Brave the Pirate King's Hideout", cost: '5 Strength, 1 Item worth 4 Gold or more', description: "2 Victory Points, 3 Gold", count: 2 },
   
   // { type: 'Adventure', victoryPoints: 4, title: "Slay the Ashen Wyrn", cost: '11 Strength, 2 Damage, 1 Mana', description: "4 Victory Points, 5 Gold", count: 1 },
   // { type: 'Adventure', victoryPoints: 5, title: "Encounter the Titan Beneath the Hills", cost: '14 Strength, 3 Cards', description: "5 Victory Points, 2 Gold, 2 Mana, 1 Damage to other players", count: 1 },
@@ -107,12 +154,12 @@ export const initialAdventureDeck = [
 ];
 
 export const characters = [
-  { type: 'Character', title: "Warrior", stats: "Strength: 4\nMax Health: 11\nMana: 0", ability: "2 Starting Gold", count: 1 },
-  { type: 'Character', title: "Rogue", stats: "Strength: 2\nMax Health: 9\nMana: 2", ability: "2 Starting Gold", count: 1 },
+  { type: 'Character', title: "Warrior", stats: "Strength: 4\nMax Health: 12\nMana: 0", ability: "2 Starting Gold", count: 1 },
+  { type: 'Character', title: "Rogue", stats: "Strength: 2\nMax Health: 10\nMana: 2", ability: "2 Starting Gold", count: 1 },
   { type: 'Character', title: "Wizard", stats: "Strength: 1\nMax Health: 8\nMana: 3", ability: "2 Starting Gold", count: 1 },
-  { type: 'Character', title: "Fighter", stats: "Strength: 3\nMax Health: 10\nMana: 1", ability: "2 Starting Gold", count: 1 },
+  { type: 'Character', title: "Fighter", stats: "Strength: 3\nMax Health: 12\nMana: 1", ability: "2 Starting Gold", count: 1 },
   { type: 'Character', title: "Arcanist", stats: "Strength: 1\nMax Health: 8\nMana: 3", ability: "2 Starting Gold", count: 1 },
-  { type: 'Character', title: "Ranger", stats: "Strength: 2\nMax Health: 9\nMana: 2", ability: "2 Starting Gold", count: 1 },
+  { type: 'Character', title: "Ranger", stats: "Strength: 2\nMax Health: 10\nMana: 2", ability: "2 Starting Gold", count: 1 },
 ]
 
 export function createDeck() {
